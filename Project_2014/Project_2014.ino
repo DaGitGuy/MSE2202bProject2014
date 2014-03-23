fd1c8e5a37cbc207128d314e813b03e9b86caf15
Kulani <kayzwane@gmail.com>
2014-03-22 22:23:13 -0400

    Ultrasonic sensor code suggestion
    Taking advantage of the while-loop functionality to improve response of
    ultrasonic sensor i.e. to have less delay in adjusting straightness of
    robot.

--- Project_2014/Project_2014.ino
+++ Project_2014/Project_2014.ino
@@ -8,3 +8,30 @@ By: Ashton Gobbo
 
 // KULANI's code:
 
+//suggestions for the ultrasonic sensor code:
+
+const int ci_Ultrasonic_Ping = 5;   //input plug
+const int ci_Ultrasonic_Data = 6;   //output plug
+
+// set up ultrasonic
+pinMode(ci_Ultrasonic_Ping, OUTPUT);
+pinMode(ci_Ultrasonic_Data, INPUT);
+
+//variables
+unsigned long ul_Echo_Time;
+
+do                                                                              
+{                                                                                           
+  Serial.println(Ping());
+  
+}while(Ping() <= 500);
+
+int Ping() //*** B-A Idea 
+{
+  digitalWrite(ci_Ultrasonic_Ping, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ci_Ultrasonic_Ping,LOW);
+  ul_Echo_Time = pulseIn (ci_Ultrasonic_Data, HIGH, 10000);
+  return ul_Echo_Time;
+}
+
