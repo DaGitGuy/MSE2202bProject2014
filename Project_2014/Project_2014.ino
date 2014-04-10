aabbde29cae251cf92d76488ce445ed85229cee4
jbwgithub <jwintrau@uwo.ca>
2014-04-09 23:34:05 -0400

    Robot Top Potion- Arm Code
    This is my final upload of the top/ arm code for the robot, that
    performs all of the functions on the upper portion of the robot (i.e.
    picking up and dropping off). Ashton made some more changes to the
    code, before finalizing the code for the design showcase.

--- Project_2014/Project_2014.ino
+++ Project_2014/Project_2014.ino
@@ -6,7 +6,7 @@ By: Ashton Gobbo
     Kulani Zwane
 */
 
-//Josh'S Code
+//Josh's Code
 
 
 #include<Servo.h>
@@ -15,7 +15,9 @@ Servo gripmotor;
 int pos=0;
 int incoming = 0; //KULANI
 int val = 0; //KULANI 
-int topindex = 0; //KULANI
+int topindex = 0; 
+int duration = 0;
+int duration1 = 0;
 
 void setup()
 {
@@ -29,11 +31,23 @@ void setup()
 }
 void loop()
 {
+  
+       if(Ping()<90 || Ping1()<90)
+      {
+       Serial.print(9);
+        }
+  
   topindex=0;
  switch (topindex)                    //KULANI
  {                                    // "
   case 0:                             // "
-  {                                   // "
+      {                                   // "
+        if(Ping()<100 || Ping1()<100)
+      {
+       Serial.print(9);
+      } 
+    
+ 
     incoming = Serial.available();    // " 
          // "
     val = Serial.parseInt();          // "
@@ -76,12 +90,8 @@ void loop()
  
  case 1:
  { //drop off
- 
- 
- 
   
-     armforward();
-     armforward();
+     armforward2();
      delay(500);
     
       for(pos = 0; pos < 60; pos += 5)  // goes from 0 degrees to 180 degrees 
@@ -110,8 +120,8 @@ void loop()
  
  case 3:
  {
-   int leftbar;
-   int rightbar;
+   int leftbar;          //The following code is the case that uses the linetrackers to read the code off of the envelopes, so that the bot knows where to deliver it.
+   int rightbar;         //The code is translated into a number representing the room number (1,2,3, or 4), and is stored to the variable "room".
    int room=0;
    leftbar= analogRead(A1);
    rightbar= analogRead(A2);
@@ -140,7 +150,7 @@ void armreverse()
   while(1)
   {
     armmotor.writeMicroseconds(1900);
-    if (millis()-time >2000)
+    if (millis()-time >2200)
     {
       armmotor.writeMicroseconds(1500);
       break;
@@ -149,23 +159,40 @@ void armreverse()
 }
 void armforward()
 { 
+  
   double time=millis();
   while( 1)
   {
-    armmotor.writeMicroseconds(1100);
-    if (millis()-time >700)
+    armmotor.writeMicroseconds(1200);
+    if (millis()-time >1800)
     {
       armmotor.writeMicroseconds(1500);
       break;
     }
   }
 }
-void armdropoff()
+
+void armforward1()
+{ 
+  
+  double time=millis();
+  while( 1)
+  {
+    armmotor.writeMicroseconds(1200);
+    if (millis()-time >600)
+    {
+      armmotor.writeMicroseconds(1500);
+      break;
+    }
+  }
+}
+
+void armforward2()            //This function is the same as armforward, but it extends the arm out twice as much as the function armforward().
 {
   double time= millis();
   while(1)
   {
-    armmotor.writeMicroseconds(1100);
+    armmotor.writeMicroseconds(1200);
     if (millis()-time>2000)
     {
       armmotor.writeMicroseconds(1500);
@@ -173,9 +200,34 @@ void armdropoff()
     }
   }
 }
-
-
-
+void armreverse2()
+{ 
+  
+  double time=millis();
+  while( 1)
+  {
+    armmotor.writeMicroseconds(1900);
+    if (millis()-time >2000)
+    {
+      armmotor.writeMicroseconds(1500);
+      break;
+    }
+  }
+}
+void armreverse1()
+{ 
+  
+  double time=millis();
+  while( 1)
+  {
+    armmotor.writeMicroseconds(1700);
+    if (millis()-time >800)
+    {
+      armmotor.writeMicroseconds(1500);
+      break;
+    }
+  }
+}
   
 double Ping()
 {
@@ -190,4 +242,19 @@ double Ping()
   //Serial.print(duration/58);
   //Serial.print(" . ");
   return duration;
+}
+
+double Ping1()
+{
+  //Ping Ultrasonic
+  //Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
+  digitalWrite(17, HIGH);
+  delayMicroseconds(10); //The 10 microsecond pause where the pulse in "high"
+  digitalWrite(17, LOW);
+  //use command pulseIn to listen to Ultrasonic_Data pin to record the
+  //time that it takes from when the Pin goes HIGH until it goes LOW 
+  duration1 = pulseIn(18, HIGH, 10000);
+  //Serial.print(duration/58);
+  //Serial.print(" . ");
+  return (duration1/58);
 }
